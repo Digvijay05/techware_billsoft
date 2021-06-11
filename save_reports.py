96fb1165b156955bfc07b0841b868ad04de95a92
Digvijay05 <djgamingttt@gmail.com>
2021-06-11 17:34:17 +0530

    Adding Reports Functionality (Not BUILT!)

--- save_reports.py
+++ save_reports.py
@@ -6,8 +6,8 @@ from threading import Thread
 import time
 import openpyxl.drawing.image
 from openpyxl import load_workbook, Workbook
+
 self = Thread()
-self.run()
 self.start()
 # Connecting To Database
 self.conn = sqlite3.connect("DB\\Business.db")
@@ -39,26 +39,32 @@ self.conn = sqlite3.connect("DB\\Invoices.db")
 self.cursor = self.conn.cursor()
 self.cursor.execute("SELECT * FROM INVOICES WHERE Invoice_No = ?", ("WF1001",))
 self.rows = self.cursor.fetchall()
-t = time.localtime(time.time())
+self.t = time.localtime(time.time())
 self.reports_workbook = Workbook()
 self.reports_sheet = self.reports_workbook.active
-self.reports_sheet.title = f"{t.tm_mon}/{t.tm_year} Reports"
+self.reports_sheet.title = f"{self.t.tm_mon}-{self.t.tm_year} Reports"
 self.row = 0
 self.col = 0
+if self.rows == [] or self.rows == None:
+    print("no data")
+else:
+    print(self.rows)
+    self.cols = ["No.", "Name", "Phone Number"]
+    for i in self.categories:
+        self.cols.append(i)
+    # Giving Column names in Reports Sheet
+    # Appending Column Name To No.
+    self.reports_sheet.cell(row=self.row + 1, column=self.col + 1,
+                            value=self.cols[0])
+    # Appending Column Name To Name
+    self.reports_sheet.cell(row=self.row + 1, column=self.col + 1, value=self.cols[1])
+    # Appending Column Name To Phone No.
+    self.reports_sheet.cell(row=self.row + 1, column=self.col + 1, value=self.rows[6])
+    # row_cat = category to append in row
+    for row_cat in self.categories:
+        # Adding Categories in Reports Dynamically
+        self.reports_sheet.cell(row=self.row + 1, column=self.col + self.reports_sheet.max_column,
+                                value=row_cat)
 
-self.cols = ["No.", "Name", "Phone Number"]
-for i in self.categories:
-    self.cols.append(i)
-# Giving Column names in Reports Sheet
-# Appending Column Name To No.
-self.reports_sheet.cell(row=self.row + 1, column=self.col + 1,
-                        value=self.cols[0])
-# Appending Column Name To Name
-self.reports_sheet.cell(row=self.row + 1, column=self.col + 1, value=self.cols[1])
-# Appending Column Name To Phone No.
-self.reports_sheet.cell(row=self.row + 1, column=self.col + 1, value=self.rows[6])
-# row_cat = category to append in row
-for row_cat in self.categories:
-    # Adding Categories in Reports Dynamically
-    self.reports_sheet.cell(row=self.row + 1, column=self.col + self.reports_sheet.max_column,
-                            value=row_cat)
+    self.reports_workbook.save(f"{self.t.tm_mon},{self.t.tm_year}.xlsx")
+    self.reports_workbook.close()
